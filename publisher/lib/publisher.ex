8d3a85e20acba9fe740c51bb84bbf6e15f074f39
TORIFUKUKaiou <torifuku.kaiou@gmail.com>
2023-06-30 11:23:17 +0900

    lib/publisher.ex

--- publisher/lib/publisher.ex
+++ publisher/lib/publisher.ex
@@ -1,18 +1,55 @@
 defmodule Publisher do
-  @moduledoc """
-  Documentation for `Publisher`.
-  """
+  use GenServer
+  require Logger
 
-  @doc """
-  Hello world.
+  def start_link(options \\ %{}) do
+    GenServer.start_link(__MODULE__, options, name: __MODULE__)
+  end
 
-  ## Examples
+  @impl true
+  def init(options) do
+    state = %{
+      interval: options[:interval] || 10_000,
+      weather_tracker_url: options[:weather_tracker_url],
+      sensors: options[:sensors],
+      measurements: :no_measurements
+    }
 
-      iex> Publisher.hello()
-      :world
+    schedule_next_publish(state.interval)
+    {:ok, state}
+  end
 
-  """
-  def hello do
-    :world
+  defp schedule_next_publish(interval) do
+    Process.send_after(self(), :publish_data, interval)
+  end
+
+  @impl true
+  def handle_info(:publish_data, state) do
+    {:noreply, state |> measure() |> publish()}
+  end
+
+  defp measure(state) do
+    measurements =
+      Enum.reduce(state.sensors, %{}, fn sensor, acc ->
+        sensor_data = sensor.read.() |> sensor.convert.()
+        Map.merge(acc, sensor_data)
+      end)
+
+    %{state | measurements: measurements}
+  end
+
+  defp publish(state) do
+    result =
+      :post
+      |> Finch.build(
+        state.weather_tracker_url,
+        [{"Content-Type", "application/json"}],
+        Jason.encode!(state.measurements)
+      )
+      |> Finch.request(WeatherTrackerClient)
+
+    Logger.debug("Server response: #{inspect(result)}")
+    schedule_next_publish(state.interval)
+    state
   end
 end
